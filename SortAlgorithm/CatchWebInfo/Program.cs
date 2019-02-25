11cb8f27510e94d4df45dc2f1f60532940c6833b
JourneyChen88 <372040524@qq.com>
2019-02-25 14:48:22 +0800

    修改排序

---
+++ SortAlgorithm/CatchWebInfo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace CatchWebInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendRequest();
+        }
+
+        //方法一
+
+        public static void webClientMethod1()
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            //以字符串的形式返回数据
+            string html = wc.DownloadString("https://www.baidu.com/");
+
+            //以正则表达式的形式匹配到字符串网页中想要的数据
+            MatchCollection matches = Regex.Matches(html, "<a.*>(.*)</a>");
+            //依次取得匹配到的数据
+            foreach (Match item in matches)
+            {
+              
+                foreach (Group info in item.Groups)
+                {
+                    Console.WriteLine(info.Value);
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        //方法二
+        public static string SendRequest()
+        {
+            string url = "https://www.baidu.com/";
+            Uri httpURL = new Uri(url);
+
+            ///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法 建立，并进行强制的类型转换   
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
+            //httpReq.Headers.Add("cityen", "tj");
+
+            ///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换   
+            HttpWebResponse httpResp = (HttpWebResponse)httpReq.GetResponse();
+
+            ///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容   
+            ///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错 误。
+            System.IO.Stream respStream = httpResp.GetResponseStream();
+
+            ///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容
+            System.IO.StreamReader respStreamReader = new System.IO.StreamReader(respStream, Encoding.UTF8);
+            //从流的当前位置读取到结尾
+            string strBuff = respStreamReader.ReadToEnd();
+
+         
+            respStreamReader.Close();
+            respStream.Close();
+            return strBuff;
+        }
+
+    }
+}
--- SortAlgorithm/Sort/Program.cs
+++ SortAlgorithm/Sort/Program.cs
@@ -6,10 +6,110 @@ using System.Threading.Tasks;
 
 namespace Sort
 {
+    //class Program
+    //{
+    //    //static void Main(string[] args)
+    //    //{
+    //    //    B b = new B();
+    //    //    b.PrintFields();
+    //    //    Console.ReadLine();
+    //    //}
+    //    Class1
+    //}
     class Program
     {
-        static void Main(string[] args)
+
+        private string str = "Class1.str";
+
+        private int i = 0;
+
+        static void StringConvert(string str)
         {
+
+            str = "string being converted.";
+
         }
+
+        static void StringConvert(Program c)
+        {
+
+            c.str = "string being converted.";
+
+        }
+
+        static void Add(int i)
+        {
+
+            i++;
+
+        }
+
+        static void AddWithRef(ref int i)
+        {
+
+            i++;
+
+        }
+
+        static void Main()
+        {
+
+            int i1 = 10;
+
+            int i2 = 20;
+
+            string str = "str";
+
+            Program c = new Program();
+
+            Add(i1);
+
+            AddWithRef(ref i2);
+
+            Add(c.i);
+
+            StringConvert(str);
+
+            StringConvert(c);
+
+            Console.WriteLine(i1);
+
+            Console.WriteLine(i2);
+
+            Console.WriteLine(c.i);
+
+            Console.WriteLine(str);
+
+            Console.WriteLine(c.str);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+    class A
+
+    {
+
+        public A()
+        {
+            PrintFields();
+        }
+        public virtual void PrintFields() { }
+    }
+
+
+    class B : A
+    {
+        int x = 1;
+        int y;
+        public B() { y = -1; }
+
+
+        public override void PrintFields()
+        { Console.WriteLine("x={0},y={1}", x, y);
+        }
+
     }
 }
